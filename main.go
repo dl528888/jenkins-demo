eddce237cad8c624856cb2fa206abcc052d188ef
DengLei <dl528888@gmail.com>
2021-05-28 23:41:11 +0800

    Update main.go

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
     "fmt"
     "log"
     "net/http"
+    "os"
 )
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello World")
+        fmt.Fprintf(w, "Hello, Kubernetes！I'm from Jenkins CI！BRANCH_NAME:%s"%os.Getenv("branch"))
     })
 
     log.Fatal(http.ListenAndServe(":8080", nil))
